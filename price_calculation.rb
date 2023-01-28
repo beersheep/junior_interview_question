4d3516f06c9672fb93476cae2e92d58b9c79cce2
Jeff Chen <zen00326598@gmail.com>
2023-01-28 22:14:25 +0800

    完成price_calculation

--- price_calculation.rb
+++ price_calculation.rb
@@ -12,11 +12,33 @@ require './models/order.rb'
 
 class PriceCalculation
   def initialize(order_id)
+    @order = verify(order_id)
+  end
+
+  def campaign
+    @best_discount = Campaign.running_campaigns(@order.order_date).map(&:discount_ratio).max
   end
 
   def total
+    if campaign
+      @discount_price = @order.price * ((100 - @best_discount )/ 100.0)
+    else
+      @discount_price = @order.price
+    end
+
+    (free_shipment?)? @discount_price : @discount_price += 60
   end
 
   def free_shipment?
+    @order.price > 1500
   end
+
+  private
+
+  def verify(order_id)
+    order = Order.find(order_id)
+    return order if order
+    raise Order::NotFound
+  end
+
 end
